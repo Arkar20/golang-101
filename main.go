bff7164af9c11d578650b700d54de9854deb0e5f
arkar20 <arkar20011@gmail.com>
2025-01-22 11:01:55 -0800

    variables, array & slices

--- main.go
+++ main.go
@@ -17,4 +17,22 @@ func main() {
 
 	fmt.Printf("My name is %v and My age is %v", name, age)
 
+	// array
+	// var ages [3]int = [3]int{12, 22, 22}
+	var ages = [3]int{12, 22, 22}
+
+	fmt.Println(ages, len(ages))
+
+	names := [3]string{"arkar", "arkar1", "arkar3"}
+	fmt.Println(names, len(names))
+
+	//slices
+	scores := []int{100, 222, 222}
+	fmt.Println("these are the scores", scores)
+
+	scores[2] = 2213213
+	fmt.Println("these are the scores", scores)
+
+	newScores := append(scores, 82)
+	fmt.Println("these are the scores", newScores)
 }
